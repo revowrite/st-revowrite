c22161b933f18360fad1c6a5d475df45a9d3d4b2
revowrite <131659859+revowrite@users.noreply.github.com>
2023-06-08 19:17:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from language_tool_python import LanguageTool
- 
+
 def correct_grammar(text):
     tool = LanguageTool('en-US')
     matches = tool.check(text)
@@ -13,10 +13,12 @@ def main():
 
     text = st.text_area("Enter text", height=200)
     if st.button("Check Grammar"):
-        corrected_text = correct_grammar(text)
-        st.subheader("Corrected Text")
-        st.write(corrected_text)
+        try:
+            corrected_text = correct_grammar(text)
+            st.subheader("Corrected Text")
+            st.write(corrected_text)
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
-
