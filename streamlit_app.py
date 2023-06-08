7f20eae6cf30a9b1b10d36eaed2dae8831ba0f8c
revowrite <131659859+revowrite@users.noreply.github.com>
2023-06-08 19:32:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,23 @@
 import streamlit as st
-from pygrammarly import GrammarlyChecker
+import spacy
 
 def correct_grammar(text):
-    checker = GrammarlyChecker()
-    corrected_text = checker.correct(text)
+    # Load the English language model in spaCy
+    nlp = spacy.load('en_core_web_sm')
+
+    # Process the text with spaCy
+    doc = nlp(text)
+
+    # Implement your grammar correction logic using spaCy's syntactic analysis
+
+    # Example rule: Correct "is not" to "isn't"
+    for token in doc:
+        if token.text == "is" and token.idx > 0 and doc[token.i-1].text.lower() == "not":
+            corrected_text = doc.text[:token.idx-1] + "n't" + doc.text[token.idx+len(token):]
+            doc = nlp(corrected_text)
+
+    corrected_text = doc.text
+
     return corrected_text
 
 def main():
