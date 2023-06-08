dd84b3cec17ca45f8708c81cce0285dcb969c88b
revowrite <131659859+revowrite@users.noreply.github.com>
2023-06-08 17:09:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,15 +13,18 @@ def extract_text_from_pdf(pdf_file):
 def compare_texts(text1, text2):
     return SequenceMatcher(None, text1, text2).ratio()
 
-def check_plagiarism(given_pdf, local_pdfs):
+def check_plagiarism(given_pdf, local_folder):
     given_text = extract_text_from_pdf(given_pdf)
 
     total_similarity = 0
-    for local_pdf in local_pdfs:
-        local_text = extract_text_from_pdf(local_pdf)
-        similarity = compare_texts(given_text, local_text)
-        st.write(f'{local_pdf.name}: {similarity * 100}%')
-        total_similarity += similarity
+    local_pdfs = [file for file in os.listdir(local_folder) if file.endswith(".pdf")]
+    for pdf in local_pdfs:
+        pdf_path = os.path.join(local_folder, pdf)
+        with open(pdf_path, 'rb') as file:
+            local_text = extract_text_from_pdf(file)
+            similarity = compare_texts(given_text, local_text)
+            st.write(f'{pdf}: {similarity * 100}%')
+            total_similarity += similarity
 
     overall_plagiarism = (total_similarity / len(local_pdfs)) * 100
     st.write(f'\nOverall Plagiarism: {overall_plagiarism}%')
@@ -32,11 +35,11 @@ st.title('Plagiarism Checker')
 # GUI for selecting the given PDF file
 given_pdf = st.file_uploader('Select Given PDF File', type='pdf')
 
-# GUI for selecting the local PDF files
-local_pdfs = st.file_uploader('Select Local PDF Files', type='pdf', accept_multiple_files=True)
+# GUI for selecting the local PDF folder
+local_folder = st.text_input('Enter Local PDF Folder Path')
 
 if st.button('Check Plagiarism'):
-    if given_pdf is not None and local_pdfs is not None:
-        check_plagiarism(given_pdf, local_pdfs)
+    if given_pdf is not None and local_folder:
+        check_plagiarism(given_pdf, local_folder)
     else:
-        st.warning('Please select the given PDF file and the local PDF files.')
+        st.warning('Please select the given PDF file and enter the local PDF folder path.')
