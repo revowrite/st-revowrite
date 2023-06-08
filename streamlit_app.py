06de28df5bcd2bbc476e427ea37798340523ed02
revowrite <131659859+revowrite@users.noreply.github.com>
2023-06-08 19:43:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,12 @@
 import streamlit as st
-import pip
-
-def install_dependencies():
-    required_packages = [
-        'streamlit',
-        'language-tool-python'
-    ]
-
-    st.write("Installing dependencies...")
-    st.empty()
-    st.info("This may take a few moments.")
-
-    for package in required_packages:
-        pip.main(['install', package])
-
-    st.success("Dependencies installed successfully.")
+from pygrammarly import GrammarlyChecker
 
 def correct_grammar(text):
-    import language_tool_python
-    tool = language_tool_python.LanguageTool('en-US')
-    matches = tool.check(text)
-    corrected_text = language_tool_python.correct(text, matches)
+    checker = GrammarlyChecker()
+    corrected_text = checker.correct(text)
     return corrected_text
 
 def main():
-    install_dependencies()
-
     st.title("Grammar Checker")
     st.write("Enter text and click the 'Check Grammar' button to correct grammar mistakes.")
 
