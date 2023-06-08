300fc7f7d23102bcaccc09d834240bc2b18ba08c
revowrite <131659859+revowrite@users.noreply.github.com>
2023-06-08 17:13:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,21 +13,17 @@ def extract_text_from_pdf(pdf_file):
 def compare_texts(text1, text2):
     return SequenceMatcher(None, text1, text2).ratio()
 
-def check_plagiarism(given_pdf, local_folder):
+def check_plagiarism(given_pdf, local_pdfs):
     given_text = extract_text_from_pdf(given_pdf)
 
     total_similarity = 0
-    for root, dirs, files in os.walk(local_folder):
-        for file in files:
-            if file.endswith(".pdf"):
-                pdf_path = os.path.join(root, file)
-                with open(pdf_path, 'rb') as file:
-                    local_text = extract_text_from_pdf(file)
-                    similarity = compare_texts(given_text, local_text)
-                    st.write(f'{file}: {similarity * 100}%')
-                    total_similarity += similarity
+    for local_pdf in local_pdfs:
+        local_text = extract_text_from_pdf(local_pdf)
+        similarity = compare_texts(given_text, local_text)
+        st.write(f'{local_pdf.name}: {similarity * 100}%')
+        total_similarity += similarity
 
-    overall_plagiarism = (total_similarity / len(files)) * 100
+    overall_plagiarism = (total_similarity / len(local_pdfs)) * 100
     st.write(f'\nOverall Plagiarism: {overall_plagiarism}%')
 
 # Streamlit app
@@ -36,11 +32,11 @@ st.title('Plagiarism Checker')
 # GUI for selecting the given PDF file
 given_pdf = st.file_uploader('Select Given PDF File', type='pdf')
 
-# GUI for selecting the local PDF folder
-local_folder = st.folder_uploader('Select Local PDF Folder')
+# GUI for selecting the local PDF files
+local_pdfs = st.file_uploader('Select Local PDF Files', type='pdf', accept_multiple_files=True)
 
 if st.button('Check Plagiarism'):
-    if given_pdf is not None and local_folder is not None:
-        check_plagiarism(given_pdf, local_folder)
+    if given_pdf is not None and local_pdfs is not None:
+        check_plagiarism(given_pdf, local_pdfs)
     else:
-        st.warning('Please select the given PDF file and the local PDF folder.')
+        st.warning('Please select the given PDF file and the local PDF files.')
