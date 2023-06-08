6cdcfeba92f4215b172e820c33b7f131ff312279
revowrite <131659859+revowrite@users.noreply.github.com>
2023-06-08 17:03:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,16 +13,14 @@ def extract_text_from_pdf(pdf_path):
 def compare_texts(text1, text2):
     return SequenceMatcher(None, text1, text2).ratio()
 
-def check_plagiarism(given_pdf, local_pdf_folder):
+def check_plagiarism(given_pdf, local_pdfs):
     given_text = extract_text_from_pdf(given_pdf.name)
-    local_pdfs = os.listdir(local_pdf_folder)
 
     total_similarity = 0
-    for pdf in local_pdfs:
-        pdf_path = os.path.join(local_pdf_folder, pdf)
-        local_text = extract_text_from_pdf(pdf_path)
+    for local_pdf in local_pdfs:
+        local_text = extract_text_from_pdf(local_pdf.name)
         similarity = compare_texts(given_text, local_text)
-        st.write(f'{pdf}: {similarity * 100}%')
+        st.write(f'{local_pdf.name}: {similarity * 100}%')
         total_similarity += similarity
 
     overall_plagiarism = (total_similarity / len(local_pdfs)) * 100
@@ -34,11 +32,11 @@ st.title('Plagiarism Checker')
 # GUI for selecting the given PDF file
 given_pdf = st.file_uploader('Select Given PDF File', type='pdf')
 
-# GUI for selecting the local PDF folder
-local_pdf_folder = st.folder_uploader('Select Local PDF Folder')
+# GUI for selecting the local PDF files
+local_pdfs = st.file_uploader('Select Local PDF Files', type='pdf', accept_multiple_files=True)
 
 if st.button('Check Plagiarism'):
-    if given_pdf is not None and local_pdf_folder is not None:
-        check_plagiarism(given_pdf, local_pdf_folder)
+    if given_pdf is not None and local_pdfs is not None:
+        check_plagiarism(given_pdf, local_pdfs)
     else:
-        st.warning('Please select the given PDF file and the local PDF folder.')
+        st.warning('Please select the given PDF file and the local PDF files.')
