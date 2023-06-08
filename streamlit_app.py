a2fd50cc6bfef2c0749f58372e2d17e1c7ff09d2
revowrite <131659859+revowrite@users.noreply.github.com>
2023-06-08 19:30:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,10 @@
 import streamlit as st
-import requests
+from language_tool_python import LanguageTool
 
 def correct_grammar(text):
-    url = "https://api.grammarbot.io/v2/check"
-    params = {
-        "text": text,
-        "language": "en-US"
-    }
-    response = requests.post(url, json=params)
-    result = response.json()
-    
-    corrected_text = text
-    matches = result.get("matches")
-    if matches:
-        for match in matches:
-            replacement = match["replacements"][0]["value"]
-            incorrect = match["context"]["text"]
-            corrected_text = corrected_text.replace(incorrect, replacement)
-    
+    tool = LanguageTool('en-US')
+    matches = tool.check(text)
+    corrected_text = LanguageTool.correct(text, matches)
     return corrected_text
 
 def main():
